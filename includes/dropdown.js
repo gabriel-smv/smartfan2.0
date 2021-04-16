9ce745472682cfa9d56a25b68aec5c545bdb22d2
Gabriel <gabriel.perez@smartmove.pt>
2021-04-16 18:24:19 -0300

    style banners + footer

---
+++ includes/dropdown.js
@@ -0,0 +1,19 @@
+// Prevent closing from click inside dropdown
+    
+document.querySelectorAll('.nav-dropdown').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+})
+
+document.querySelectorAll('.user-dropdown').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+})
+
+document.querySelectorAll('.fan-dropdown').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+})
