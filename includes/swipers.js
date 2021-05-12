cde85012feaf48965eb475af350ae0204308e9e4
Gabriel <gabriel.perez@smartmove.pt>
2021-05-12 19:06:15 -0300

    Area fan + icons

---
+++ includes/swipers.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-area-fan', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
